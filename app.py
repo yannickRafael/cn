c857d9a1b4ac5002f18676e957ae1349519f0b4f
yannickRafael <yannickrafael286@gmail.com>
2025-06-27 12:44:54 +0200

    modified port and host

--- app.py
+++ app.py
@@ -12,5 +12,5 @@ def download_file():
         abort(404, description="Arquivo não encontrado.")
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
